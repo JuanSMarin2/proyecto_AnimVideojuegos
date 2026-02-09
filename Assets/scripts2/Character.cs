c2fa7a3ed946fdeffa7e6e3d70b09a16de71e3c0
LS1653 <estebanpuerta2006@gmail.com>
2026-02-09 15:33:40 -0500

    clase-5
    Falta-toda-la-clase-xd

---
+++ Assets/scripts2/Character.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Assets.scripts2
+{
+    public class Character : MonoBehaviour
+    {
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Awake()
+        {
+            RegistreComponent();
+        }
+
+        // Update is called once per frame
+        private void RegistreComponent()
+        {
+           
+        }
+    }
+}
+
+
---
+++ Assets/scripts2/CharacterLook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.scripts2
+{   
+
+    public class CharacterLook: MonoBehaviour, ICharacterComponent
+    {
+        [Serializable] private Camera camera;
+        public void OnLock(InputAction.CallbackContext ctx)
+        {
+            if (!ctx.started)
+            {
+                Debug.Log("Lock");
+            }
+        }
+        Character ParentCharacter { get; set; }
+    }
+}
