7d976fd2bc7a6f82d73ffa3be71a3dd0d185c211
JuanSMarin2 <160736940+JuanSMarin2@users.noreply.github.com>
2026-03-11 14:17:21 -0500

    Implementacion de crouch image

--- Assets/Entrega 2/Esteban/Scripts/CharacterCrouch.cs
+++ Assets/Entrega 2/Esteban/Scripts/CharacterCrouch.cs
@@ -6,6 +6,7 @@ public class CharacterCrouch : MonoBehaviour, ICharacterComponent
     public Character ParentCharacter { get; set; }
 
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject crouchImage;
 
     private void Awake()
     {
@@ -13,13 +14,18 @@ public class CharacterCrouch : MonoBehaviour, ICharacterComponent
         ParentCharacter.IsCrouching = false;
     }
 
-  
+
     public void OnCrouch(InputAction.CallbackContext ctx)
-     {
+    {
         if (!ctx.performed) return;
 
         ParentCharacter.IsCrouching = !ParentCharacter.IsCrouching;
         animator.SetBool("isCrouching", ParentCharacter.IsCrouching);
-       
-     }
+
+        if (crouchImage != null)
+        {
+            crouchImage.SetActive(ParentCharacter.IsCrouching);
+
+        }
     }
+}
