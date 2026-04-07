8dd138270fea44c24cd5efab999e655d391b6d4c
LS1653 <estebanpuerta2006@gmail.com>
2026-04-07 17:35:57 -0500

    clase-11-mitad

---
+++ Assets/Clases/Clase 2/Scripts/LocalPosClampConstraint.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine; 
+using UnityEngine.Animations.Rigging;
+
+namespace Assets.Clases.Clase_2.Scripts
+{
+    [AddComponentMenu("Animation Rigging/Custom/Local Pos Clamp Constraint")]
+   public class LocalPosClampConstraint: RigConstraint<LocalPosClampJob, LocalPosClamptData, LocalPosClamptBinder>
+    {
+        
+    }
+
+}
---
+++ Assets/Clases/Clase 2/Scripts/LocalPosClampJob.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.Animations.Rigging;
+using Unity.Burst;
+using UnityEngine.Animations;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace Assets.Clases.Clase_2.Scripts
+{
+    [BurstCompile]
+    internal struct LocalPosClampJob : IWeightedAnimationJob
+    {
+
+        public ReadWriteTransformHandle driven;
+
+        public float3 minLocal;
+        public float3 maxLocal;
+        public FloatProperty jobWeight { get; set; }
+
+        public void ProcessAnimation(AnimationStream stream)
+        {
+
+        }
+
+        public void ProcessRootMotion(AnimationStream stream)
+        {
+            float w = jobWeight.Get(stream);
+
+            if (w == 0f) return;
+
+            float3 lP = driven.GetLocalPosition(stream);
+            float3 clamped = math.clamp(lP, minLocal, maxLocal);
+            float3 res= math.lerp(lP, clamped, w);
+
+            driven.SetLocalPosition(stream, res);   
+        }
+    }
+
+    [System.Serializable]
+    public struct LocalPosClamptData : IAnimationJobData
+    {
+        [SyncSceneToStream] public Transform constrainedObject;
+
+        public Vector3 minLocal;
+        public Vector3 maxLocal;
+
+        public bool IsValid() => constrainedObject != null && 
+                                 minLocal.x <= maxLocal.x && 
+                                 minLocal.y <= maxLocal.y && 
+                                 minLocal.z <= maxLocal.z;                                                                     
+
+        public void SetDefaultValues()
+        {
+            constrainedObject = null;
+            minLocal = new Vector3(-0.05f, -0.05f, -0.05f);
+            maxLocal = new Vector3(0.05f, 0.05f, 0.05f);
+        }
+    }
+
+    public class LocalPosClamptBinder : AnimationJobBinder<LocalPosClampJob, LocalPosClamptData>
+    {
+        public override LocalPosClampJob Create(Animator animator, ref LocalPosClamptData data, Component component)
+        {
+            LocalPosClampJob job = new LocalPosClampJob();
+            job.driven = ReadWriteTransformHandle.Bind(animator, data.constrainedObject);
+            job.minLocal = data.minLocal;
+            job.maxLocal = data.maxLocal;
+            return job;
+        }
+    }
+}
